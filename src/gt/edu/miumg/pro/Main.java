94f1ff67a6fc29b5fcd55640a42c9205b466a584
keilylopez22 <keilysamanta125@gmail.com>
2024-09-19 21:03:56 -0600

    repasods

---
+++ src/gt/edu/miumg/pro/Main.java
@@ -0,0 +1,7 @@
+package gt.edu.miumg.pro;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/gt/edu/miumg/pro/Repasodos.java
@@ -0,0 +1,5 @@
+package gt.edu.miumg.pro;
+
+public class Repasodos {
+
+}
